553e51f237d264497d8c2a8208057216fbe30e0e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-29 17:48:25 +0300

    Initial Commit

---
+++ CircularSlider/CircularSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSliderApp.swift
+//  CircularSlider
+//
+//  Created by Yunus Emre Taşdemir on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
